8c4f5b642dae4dd3ff2aad912812d276df8fe95f
suyiiyii <suyiiyii@gmail.com>
2025-07-31 13:34:21 +0000

    add hypernode_validation_test

---
+++ test/e2e/admission/hypernode_validation_test.go
@@ -0,0 +1,448 @@
+/*
+Copyright 2024 The Volcano Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package admission
+
+import (
+	"context"
+
+	"github.com/onsi/ginkgo/v2"
+	"github.com/onsi/gomega"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	hypernodev1alpha1 "volcano.sh/apis/pkg/apis/topology/v1alpha1"
+	"volcano.sh/volcano/test/e2e/util"
+)
+
+var _ = ginkgo.Describe("HyperNode Validating Webhook E2E Test", func() {
+
+	ginkgo.It("Should allow HyperNode creation with valid exactMatch selector", func() {
+		hyperNodeName := "valid-exact-match-hypernode"
+		testCtx := util.InitTestContext(util.Options{})
+		defer util.CleanupTestContext(testCtx)
+
+		hyperNode := &hypernodev1alpha1.HyperNode{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: hyperNodeName,
+			},
+			Spec: hypernodev1alpha1.HyperNodeSpec{
+				Tier: 1,
+				Members: []hypernodev1alpha1.MemberSpec{
+					{
+						Type: hypernodev1alpha1.MemberTypeNode,
+						Selector: hypernodev1alpha1.MemberSelector{
+							ExactMatch: &hypernodev1alpha1.ExactMatch{
+								Name: "node-1",
+							},
+						},
+					},
+				},
+			},
+		}
+
+		_, err := testCtx.Vcclient.TopologyV1alpha1().HyperNodes().Create(context.TODO(), hyperNode, metav1.CreateOptions{})
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+		// Cleanup
+		err = testCtx.Vcclient.TopologyV1alpha1().HyperNodes().Delete(context.TODO(), hyperNodeName, metav1.DeleteOptions{})
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+	})
+
+	ginkgo.It("Should allow HyperNode creation with valid regexMatch selector", func() {
+		hyperNodeName := "valid-regex-match-hypernode"
+		testCtx := util.InitTestContext(util.Options{})
+		defer util.CleanupTestContext(testCtx)
+
+		hyperNode := &hypernodev1alpha1.HyperNode{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: hyperNodeName,
+			},
+			Spec: hypernodev1alpha1.HyperNodeSpec{
+				Tier: 1,
+				Members: []hypernodev1alpha1.MemberSpec{
+					{
+						Type: hypernodev1alpha1.MemberTypeNode,
+						Selector: hypernodev1alpha1.MemberSelector{
+							RegexMatch: &hypernodev1alpha1.RegexMatch{
+								Pattern: "node-[0-9]+",
+							},
+						},
+					},
+				},
+			},
+		}
+
+		_, err := testCtx.Vcclient.TopologyV1alpha1().HyperNodes().Create(context.TODO(), hyperNode, metav1.CreateOptions{})
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+		// Cleanup
+		err = testCtx.Vcclient.TopologyV1alpha1().HyperNodes().Delete(context.TODO(), hyperNodeName, metav1.DeleteOptions{})
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+	})
+
+	ginkgo.It("Should allow HyperNode creation with valid labelMatch selector", func() {
+		hyperNodeName := "valid-label-match-hypernode"
+		testCtx := util.InitTestContext(util.Options{})
+		defer util.CleanupTestContext(testCtx)
+
+		hyperNode := &hypernodev1alpha1.HyperNode{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: hyperNodeName,
+			},
+			Spec: hypernodev1alpha1.HyperNodeSpec{
+				Tier: 1,
+				Members: []hypernodev1alpha1.MemberSpec{
+					{
+						Type: hypernodev1alpha1.MemberTypeNode,
+						Selector: hypernodev1alpha1.MemberSelector{
+							LabelMatch: &metav1.LabelSelector{
+								MatchLabels: map[string]string{
+									"topology-rack": "rack1",
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+
+		_, err := testCtx.Vcclient.TopologyV1alpha1().HyperNodes().Create(context.TODO(), hyperNode, metav1.CreateOptions{})
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+		// Cleanup
+		err = testCtx.Vcclient.TopologyV1alpha1().HyperNodes().Delete(context.TODO(), hyperNodeName, metav1.DeleteOptions{})
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+	})
+
+	ginkgo.It("Should reject HyperNode creation with empty exactMatch name", func() {
+		hyperNodeName := "invalid-empty-exact-match"
+		testCtx := util.InitTestContext(util.Options{})
+		defer util.CleanupTestContext(testCtx)
+
+		hyperNode := &hypernodev1alpha1.HyperNode{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: hyperNodeName,
+			},
+			Spec: hypernodev1alpha1.HyperNodeSpec{
+				Tier: 1,
+				Members: []hypernodev1alpha1.MemberSpec{
+					{
+						Type: hypernodev1alpha1.MemberTypeNode,
+						Selector: hypernodev1alpha1.MemberSelector{
+							ExactMatch: &hypernodev1alpha1.ExactMatch{
+								Name: "",
+							},
+						},
+					},
+				},
+			},
+		}
+
+		_, err := testCtx.Vcclient.TopologyV1alpha1().HyperNodes().Create(context.TODO(), hyperNode, metav1.CreateOptions{})
+		gomega.Expect(err).To(gomega.HaveOccurred())
+		gomega.Expect(err.Error()).To(gomega.ContainSubstring("member exactMatch name is required"))
+	})
+
+	ginkgo.It("Should reject HyperNode creation with empty regexMatch pattern", func() {
+		hyperNodeName := "invalid-empty-regex-pattern"
+		testCtx := util.InitTestContext(util.Options{})
+		defer util.CleanupTestContext(testCtx)
+
+		hyperNode := &hypernodev1alpha1.HyperNode{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: hyperNodeName,
+			},
+			Spec: hypernodev1alpha1.HyperNodeSpec{
+				Tier: 1,
+				Members: []hypernodev1alpha1.MemberSpec{
+					{
+						Type: hypernodev1alpha1.MemberTypeNode,
+						Selector: hypernodev1alpha1.MemberSelector{
+							RegexMatch: &hypernodev1alpha1.RegexMatch{
+								Pattern: "",
+							},
+						},
+					},
+				},
+			},
+		}
+
+		_, err := testCtx.Vcclient.TopologyV1alpha1().HyperNodes().Create(context.TODO(), hyperNode, metav1.CreateOptions{})
+		gomega.Expect(err).To(gomega.HaveOccurred())
+		gomega.Expect(err.Error()).To(gomega.ContainSubstring("member regexMatch pattern is required"))
+	})
+
+	ginkgo.It("Should reject HyperNode creation with invalid regex pattern", func() {
+		hyperNodeName := "invalid-regex-pattern"
+		testCtx := util.InitTestContext(util.Options{})
+		defer util.CleanupTestContext(testCtx)
+
+		hyperNode := &hypernodev1alpha1.HyperNode{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: hyperNodeName,
+			},
+			Spec: hypernodev1alpha1.HyperNodeSpec{
+				Tier: 1,
+				Members: []hypernodev1alpha1.MemberSpec{
+					{
+						Type: hypernodev1alpha1.MemberTypeNode,
+						Selector: hypernodev1alpha1.MemberSelector{
+							RegexMatch: &hypernodev1alpha1.RegexMatch{
+								Pattern: "a(b",
+							},
+						},
+					},
+				},
+			},
+		}
+
+		_, err := testCtx.Vcclient.TopologyV1alpha1().HyperNodes().Create(context.TODO(), hyperNode, metav1.CreateOptions{})
+		gomega.Expect(err).To(gomega.HaveOccurred())
+		gomega.Expect(err.Error()).To(gomega.ContainSubstring("member regexMatch pattern is invalid"))
+	})
+
+	// validate by crd  e.g. member selector must have one of exactMatch, regexMatch, or labelMatch
+	ginkgo.XIt("Should reject HyperNode creation with multiple selector types (exactMatch and regexMatch)", func() {
+		hyperNodeName := "invalid-multiple-selectors"
+		testCtx := util.InitTestContext(util.Options{})
+		defer util.CleanupTestContext(testCtx)
+
+		hyperNode := &hypernodev1alpha1.HyperNode{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: hyperNodeName,
+			},
+			Spec: hypernodev1alpha1.HyperNodeSpec{
+				Tier: 1,
+				Members: []hypernodev1alpha1.MemberSpec{
+					{
+						Type: hypernodev1alpha1.MemberTypeNode,
+						Selector: hypernodev1alpha1.MemberSelector{
+							ExactMatch: &hypernodev1alpha1.ExactMatch{
+								Name: "node-1",
+							},
+							RegexMatch: &hypernodev1alpha1.RegexMatch{
+								Pattern: "node-[0-9]+",
+							},
+						},
+					},
+				},
+			},
+		}
+
+		_, err := testCtx.Vcclient.TopologyV1alpha1().HyperNodes().Create(context.TODO(), hyperNode, metav1.CreateOptions{})
+		gomega.Expect(err).To(gomega.HaveOccurred())
+		gomega.Expect(err.Error()).To(gomega.ContainSubstring("cannot specify more than one selector type"))
+	})
+
+	// validate by crd  e.g. member selector must have one of exactMatch, regexMatch, or labelMatch
+	ginkgo.XIt("Should reject HyperNode creation with multiple selector types (exactMatch and labelMatch)", func() {
+		hyperNodeName := "invalid-multiple-selectors-exact-label"
+		testCtx := util.InitTestContext(util.Options{})
+		defer util.CleanupTestContext(testCtx)
+
+		hyperNode := &hypernodev1alpha1.HyperNode{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: hyperNodeName,
+			},
+			Spec: hypernodev1alpha1.HyperNodeSpec{
+				Tier: 1,
+				Members: []hypernodev1alpha1.MemberSpec{
+					{
+						Type: hypernodev1alpha1.MemberTypeNode,
+						Selector: hypernodev1alpha1.MemberSelector{
+							ExactMatch: &hypernodev1alpha1.ExactMatch{
+								Name: "node-1",
+							},
+							LabelMatch: &metav1.LabelSelector{
+								MatchLabels: map[string]string{
+									"topology-rack": "rack1",
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+
+		_, err := testCtx.Vcclient.TopologyV1alpha1().HyperNodes().Create(context.TODO(), hyperNode, metav1.CreateOptions{})
+		gomega.Expect(err).To(gomega.HaveOccurred())
+		gomega.Expect(err.Error()).To(gomega.ContainSubstring("cannot specify more than one selector type"))
+	})
+
+	// validate by crd  e.g. member selector must have one of exactMatch, regexMatch, or labelMatch
+	ginkgo.XIt("Should reject HyperNode creation with no selector type", func() {
+		hyperNodeName := "invalid-no-selector"
+		testCtx := util.InitTestContext(util.Options{})
+		defer util.CleanupTestContext(testCtx)
+
+		hyperNode := &hypernodev1alpha1.HyperNode{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: hyperNodeName,
+			},
+			Spec: hypernodev1alpha1.HyperNodeSpec{
+				Tier: 1,
+				Members: []hypernodev1alpha1.MemberSpec{
+					{
+						Type:     hypernodev1alpha1.MemberTypeNode,
+						Selector: hypernodev1alpha1.MemberSelector{},
+					},
+				},
+			},
+		}
+
+		_, err := testCtx.Vcclient.TopologyV1alpha1().HyperNodes().Create(context.TODO(), hyperNode, metav1.CreateOptions{})
+		gomega.Expect(err).To(gomega.HaveOccurred())
+		gomega.Expect(err.Error()).To(gomega.ContainSubstring("member selector must have one of exactMatch, regexMatch, or labelMatch"))
+	})
+
+	ginkgo.It("Should reject HyperNode creation with no members", func() {
+		hyperNodeName := "invalid-no-members"
+		testCtx := util.InitTestContext(util.Options{})
+		defer util.CleanupTestContext(testCtx)
+
+		hyperNode := &hypernodev1alpha1.HyperNode{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: hyperNodeName,
+			},
+			Spec: hypernodev1alpha1.HyperNodeSpec{
+				Tier:    1,
+				Members: []hypernodev1alpha1.MemberSpec{},
+			},
+		}
+
+		_, err := testCtx.Vcclient.TopologyV1alpha1().HyperNodes().Create(context.TODO(), hyperNode, metav1.CreateOptions{})
+		gomega.Expect(err).To(gomega.HaveOccurred())
+		gomega.Expect(err.Error()).To(gomega.ContainSubstring("member must have at least one member"))
+	})
+
+	ginkgo.It("Should reject HyperNode creation with invalid exactMatch name (invalid DNS name)", func() {
+		hyperNodeName := "invalid-dns-name"
+		testCtx := util.InitTestContext(util.Options{})
+		defer util.CleanupTestContext(testCtx)
+
+		hyperNode := &hypernodev1alpha1.HyperNode{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: hyperNodeName,
+			},
+			Spec: hypernodev1alpha1.HyperNodeSpec{
+				Tier: 1,
+				Members: []hypernodev1alpha1.MemberSpec{
+					{
+						Type: hypernodev1alpha1.MemberTypeNode,
+						Selector: hypernodev1alpha1.MemberSelector{
+							ExactMatch: &hypernodev1alpha1.ExactMatch{
+								Name: "Node_with_invalid@characters",
+							},
+						},
+					},
+				},
+			},
+		}
+
+		_, err := testCtx.Vcclient.TopologyV1alpha1().HyperNodes().Create(context.TODO(), hyperNode, metav1.CreateOptions{})
+		gomega.Expect(err).To(gomega.HaveOccurred())
+		gomega.Expect(err.Error()).To(gomega.ContainSubstring("member exactMatch validate failed"))
+	})
+
+	ginkgo.It("Should allow HyperNode update with valid changes", func() {
+		hyperNodeName := "update-test-hypernode"
+		testCtx := util.InitTestContext(util.Options{})
+		defer util.CleanupTestContext(testCtx)
+
+		// Create initial HyperNode
+		hyperNode := &hypernodev1alpha1.HyperNode{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: hyperNodeName,
+			},
+			Spec: hypernodev1alpha1.HyperNodeSpec{
+				Tier: 1,
+				Members: []hypernodev1alpha1.MemberSpec{
+					{
+						Type: hypernodev1alpha1.MemberTypeNode,
+						Selector: hypernodev1alpha1.MemberSelector{
+							ExactMatch: &hypernodev1alpha1.ExactMatch{
+								Name: "node-1",
+							},
+						},
+					},
+				},
+			},
+		}
+
+		createdHyperNode, err := testCtx.Vcclient.TopologyV1alpha1().HyperNodes().Create(context.TODO(), hyperNode, metav1.CreateOptions{})
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+		// Update HyperNode with valid changes
+		createdHyperNode.Spec.Members = []hypernodev1alpha1.MemberSpec{
+			{
+				Type: hypernodev1alpha1.MemberTypeNode,
+				Selector: hypernodev1alpha1.MemberSelector{
+					RegexMatch: &hypernodev1alpha1.RegexMatch{
+						Pattern: "node-[1-5]",
+					},
+				},
+			},
+		}
+
+		_, err = testCtx.Vcclient.TopologyV1alpha1().HyperNodes().Update(context.TODO(), createdHyperNode, metav1.UpdateOptions{})
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+		// Cleanup
+		err = testCtx.Vcclient.TopologyV1alpha1().HyperNodes().Delete(context.TODO(), hyperNodeName, metav1.DeleteOptions{})
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+	})
+
+	ginkgo.It("Should reject HyperNode update with invalid changes", func() {
+		hyperNodeName := "update-invalid-test-hypernode"
+		testCtx := util.InitTestContext(util.Options{})
+		defer util.CleanupTestContext(testCtx)
+
+		// Create initial HyperNode
+		hyperNode := &hypernodev1alpha1.HyperNode{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: hyperNodeName,
+			},
+			Spec: hypernodev1alpha1.HyperNodeSpec{
+				Tier: 1,
+				Members: []hypernodev1alpha1.MemberSpec{
+					{
+						Type: hypernodev1alpha1.MemberTypeNode,
+						Selector: hypernodev1alpha1.MemberSelector{
+							ExactMatch: &hypernodev1alpha1.ExactMatch{
+								Name: "node-1",
+							},
+						},
+					},
+				},
+			},
+		}
+
+		createdHyperNode, err := testCtx.Vcclient.TopologyV1alpha1().HyperNodes().Create(context.TODO(), hyperNode, metav1.CreateOptions{})
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+		// Update HyperNode with invalid changes (empty members)
+		createdHyperNode.Spec.Members = []hypernodev1alpha1.MemberSpec{}
+
+		_, err = testCtx.Vcclient.TopologyV1alpha1().HyperNodes().Update(context.TODO(), createdHyperNode, metav1.UpdateOptions{})
+		gomega.Expect(err).To(gomega.HaveOccurred())
+		gomega.Expect(err.Error()).To(gomega.ContainSubstring("member must have at least one member"))
+
+		// Cleanup
+		err = testCtx.Vcclient.TopologyV1alpha1().HyperNodes().Delete(context.TODO(), hyperNodeName, metav1.DeleteOptions{})
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+	})
+})
