13304a80ef0d2aed8d2f1d74407bc671e306613e
suyiiyii <suyiiyii@gmail.com>
2025-08-01 09:33:01 +0000

    Add job validation policy and update admission policy installation script

--- hack/run-e2e-kind.sh
+++ hack/run-e2e-kind.sh
@@ -37,6 +37,7 @@ function install-admission-policys {
   kubectl apply -f "pkg/webhooks/admission/pods/policies/validating-admission-policy.yaml"
   kubectl apply -f "pkg/webhooks/admission/queues/policies/validating-admission-policy.yaml"
   kubectl apply -f "pkg/webhooks/admission/jobflows/policies/validating-admission-policy.yaml"
+  kubectl apply -f "pkg/webhooks/admission/jobs/policies/validating-admission-policy.yaml"
 }
 
 function install-volcano {
