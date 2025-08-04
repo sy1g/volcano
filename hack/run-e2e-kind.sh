eddf07b9c288bb29326746e34bc3670fcaf134c3
suyiiyii <suyiiyii@gmail.com>
2025-08-04 13:20:49 +0000

    add jobs/validate

--- hack/run-e2e-kind.sh
+++ hack/run-e2e-kind.sh
@@ -95,7 +95,7 @@ custom:
   default_ns:
     node-role.kubernetes.io/control-plane: ""
   scheduler_feature_gates: ${FEATURE_GATES}
-  enabled_admissions: "/pods/mutate,/queues/mutate,/podgroups/mutate,/jobs/mutate,/podgroups/validate"
+  enabled_admissions: "/pods/mutate,/queues/mutate,/podgroups/mutate,/jobs/mutate,/jobs/validate,/podgroups/validate"
 EOF
   install-admission-policys
 }
