149b23ccfc82e5c80a605e3a6ff061008820ae57
suyiiyii <suyiiyii@gmail.com>
2025-08-05 06:17:43 +0000

    Revert "del ,/podgroups/validate"
    This reverts commit 97871eed0c6fe5fe8fbe74cb46728aad83526e0f.

--- hack/run-e2e-kind.sh
+++ hack/run-e2e-kind.sh
@@ -96,7 +96,7 @@ custom:
   default_ns:
     node-role.kubernetes.io/control-plane: ""
   scheduler_feature_gates: ${FEATURE_GATES}
-  enabled_admissions: "/pods/mutate,/queues/mutate,/podgroups/mutate,/jobs/mutate"
+  enabled_admissions: "/pods/mutate,/queues/mutate,/podgroups/mutate,/jobs/mutate,/podgroups/validate"
 EOF
   install-admission-policys
 }
