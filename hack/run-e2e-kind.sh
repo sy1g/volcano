0bd651207f7ceffec57bd000f04afcccae002338
suyiiyii <suyiiyii@gmail.com>
2025-09-03 23:55:56 +0800

    add webhook for all test

--- hack/run-e2e-kind.sh
+++ hack/run-e2e-kind.sh
@@ -237,6 +237,7 @@ custom:
   default_ns:
     node-role.kubernetes.io/control-plane: ""
   scheduler_feature_gates: ${FEATURE_GATES}
+  enabled_admissions: "/pods/mutate,/queues/mutate,/podgroups/mutate,/jobs/mutate,/jobs/validate,/jobflows/validate,/pods/validate,/queues/validate,/podgroups/validate,/hypernodes/validate"
   ignored_provisioners: ${IGNORED_PROVISIONERS:-""}
 EOF
   ;;
